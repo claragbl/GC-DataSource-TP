0bdbcda4b70e5e7d06a39c2ba20ec71ffcb52e81
Clara Gaubil <clara.gaubil@gmail.com>
2023-09-19 16:50:27 +0200

    google analytics commit

--- Hello.py
+++ Hello.py
@@ -4,4 +4,14 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    return "<p>Hello, World!</p>"
+    var = """ <!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-SJDP26RN0B"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-SJDP26RN0B');
+</script>"""
+
+    return "<p>Hello, World!</p>" +var 
