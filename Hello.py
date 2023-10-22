f61ae8cd29e5161309de8fb07bf2dbf2924b76f3
Clara Gaubil <clara.gaubil@gmail.com>
2023-10-22 19:48:58 +0200

    TP3 - google trend done

--- Hello.py
+++ Hello.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, render_template
+from flask import Flask, request, render_template, jsonify
 import logging
 import os
 from google.analytics.data_v1beta import BetaAnalyticsDataClient
@@ -9,10 +9,18 @@ from google.analytics.data_v1beta.types import (
     MetricType,
     RunReportRequest,
 )
+from matplotlib import pyplot as plt
+import matplotlib
+matplotlib.use('agg')
+from pytrends.request import TrendReq
+import base64
+from io import BytesIO
 
 KEY_FILE_LOCATION = 'datasourceproject-1df797562433.json'
 
 app = Flask(__name__)
+
+# ----------------------------------------------------------------------------------
 
 def get_active_users(property_id="YOUR-GA4-PROPERTY-ID"):
     
@@ -35,19 +43,25 @@ def get_active_users(property_id="YOUR-GA4-PROPERTY-ID"):
 
     return response.rows[0].metric_values[0].value
 
-
+# -------------------------------------------
+# MAIN PAGE (TP1 - TP2): 
+# -> GOOGLE BUTTON
+# -> GOOGLE ANALYTIC BUTTON
+# -> COOKIES
+# -> ACTIVE USERS
+# -------------------------------------------
 @app.route("/")
 def hello_world():
 
   cookies = request.cookies
-  #status_code = request.status_code
-
-  print(cookies)
   active_users = get_active_users()
-  #print(status_code)
 
   return render_template('helloTemplate.html', cookies=cookies, active_users=active_users)
 
+# -------------------------------------------
+# LOGGER PAGE (TP1):
+# -> TEXT BOX
+# -------------------------------------------
 @app.route("/logger", methods=['GET', 'POST'])
 def logger():
 #     # display a log on the server side
@@ -69,3 +83,32 @@ def logger():
     print(text)
   
   return render_template('index.html')
+
+# -------------------------------------------
+# GOOGLE TREND PAGE (TP3):
+# -> GOOGLE TREND GRAPH OF WAR & COVID
+# -------------------------------------------
+@app.route('/google-trends', methods=['GET'])
+def get_google_trends():
+
+  pytrends = TrendReq(hl='en-US', tz=360) 
+  pytrends.build_payload(kw_list=['war', 'covid'], timeframe='2019-03-01 2020-10-31')
+  data = pytrends.interest_over_time()
+    
+  # Generate a line plot to see the trends
+  plt.figure(figsize=(10, 4))
+  plt.plot(data.index, data['war'], label='War')
+  plt.plot(data.index, data['covid'], label='Covid')
+  plt.xlabel('Date')
+  plt.ylabel('Search Interest')
+  plt.title('Google Trends Data')
+  plt.legend()
+
+  # Convert the plot to a bytes object and then to base64 for embedding in a web page
+  buffer = BytesIO()
+  plt.savefig(buffer, format='png')
+  plt.close()
+  buffer.seek(0)
+  plot_data = base64.b64encode(buffer.read()).decode()
+    
+  return render_template('google_trend.html', plot_data=plot_data)
